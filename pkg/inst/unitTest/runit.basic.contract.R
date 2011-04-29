c220b83841710a4c381159fd37530a0c81effee2
mariotomo (drs ) <mariotomo@gmail.com>
2011-04-29 08:39:27 +0000

    work in progress
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gnupg@4 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/inst/unitTest/runit.basic.contract.R
+++ pkg/inst/unitTest/runit.basic.contract.R
@@ -5,6 +5,20 @@ require(svUnit)
 
 test.000.canCreateMainObject <- function() {
   gpg <- GnuPG$new(homedir=".")
-  assertEquals("gnupg", class(gpg))
+  checkEquals("gnupg", as.character(class(gpg)))
 }
 
+test.010.mainObjectHasPublicKey <- function() {
+  gpg <- GnuPG$new(homedir=".")
+  fail("not implemented yet")
+}
+
+test.100.encrypt.decrypt <- function() {
+  gpg <- GnuPG$new(homedir=".")
+  checkEquals("some text", gpg$decrypt(gpg$encrypt("some text", armor=TRUE)))
+}
+
+test.200.sign.verify <- function() {
+  gpg <- GnuPG$new(homedir=".")
+  checkTrue(gpg$verify(gpg$clearsign("some text", armor=TRUE, passphrase="")))
+}
